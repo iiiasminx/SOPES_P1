d65001ff14d85c12f5ec6b3ee3a1b69d895a9d39
Yasmin <yasminmonterrosoe@gmail.com>
2022-03-11 01:46:46 -0600

    Segundo RAM

--- ram.c
+++ ram.c
@@ -13,5 +13,31 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yásmin Monterroso");
-MODULE_DESCRIPTION("Modulo RAM - Proyecto 1");
+MODULE_DESCRIPTION("ram_grupo18");
 MODULE_VERSION("0.01");
+
+static int escribir_a_proc(struct seq_file *file_proc, void *v) {       
+
+    seq_printf(file_proc,"{\"Total_RAM\":\"\",\n\"Uso_RAM\":\"\",\n\"Porcentaje\":\"\",\n\"Libre_RAM\":\"\",\n }");
+ 
+    return 0;
+}
+
+static int abrir_aproc(struct inode *inode, struct  file *file) {
+  return single_open(file, escribir_a_proc, NULL);
+}
+
+static struct proc_ops archivo_operaciones = {    
+    .proc_open = abrir_aproc,
+    .proc_read = seq_read
+};
+
+static int __init escribir_archivo(void) {
+    proc_create("timestamps", 0, NULL, &archivo_operaciones);
+    printk(KERN_INFO "Archivo creado\n");
+    return 0;
+}
+
+/* Llamada a las cosas */
+
+module_init(escribir_archivo);
